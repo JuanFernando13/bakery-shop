11a62ee69394a3e579bd69375ffee745b214bd94
Juan Fernando <juan_fer-13@hotmail.com>
2021-11-28 16:12:50 -0500

    Primera vez con database y redux

---
+++ features/cart/amountReducer.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  totalAmount: 0
+}
+
+export const amountSlice = createSlice({
+  name: 'amount',
+  initialState,
+  reducers: {
+    incrementTotalAmount: state => {
+      state.totalAmount += 1
+    },
+    decrementTotalAmount: state => {
+      state.totalAmount -= 1
+    }
+  }
+})
+
+export const { incrementTotalAmount, decrementTotalAmount } = amountSlice.actions
+
+export default amountSlice.reducer
--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,17 @@ import NavBar from '../components/NavBar'
 import ProductCard from '../components/ProductCard'
 import styleCard from '../components/ProductCard/ProductCard.module.css'
 import RenderCards from '../components/RenderCards'
+import dbConnect from '../database/connect'
+import Products from '../models/Products'
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <>
       <NavBar />
       <RenderCards>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {products.map(({ id, description }) => (
+          <ProductCard key={id} description={description} id={id}/>
+        ))}
         <button
           className={styleCard.add}
           style={{
@@ -28,4 +29,17 @@ export default function Home() {
       </RenderCards>
     </>
   )
+}
+
+export async function getServerSideProps() {
+  await dbConnect()
+
+  const result = await Products.find({})
+  const products = result.map(doc => {
+    const product = doc.toObject()
+    product._id = `${product._id}`
+    return product
+  })
+
+  return { props: { products } }
 }
